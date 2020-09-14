52efbfb77f4b5222821b0030d1becbd1f4257c9a
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-09-14 12:19:39 -0500

    EPI 5.10 Solution without additional storage

--- arrays/permute_elements_in_array.py
+++ arrays/permute_elements_in_array.py
@@ -14,12 +14,18 @@ class Solution:
 
         return B
 
+    """ Does not use additional storage, however does update the perms array"""
+    """ Time Complexity O(n), Space Complexity O(n) """
+    def permute_elements_array(self, perms : List[int], s : List[str]) -> str:
+        # [c,b,a,d] [1,0, 2, 3]
+        # [b,c,a,d] [0,1,2,3]
+        for i in range(len(s)):
+            while perms[i] != i:
+                s[i], s[perms[i]] = s[perms[i]], s[i]
+                perms[i], perms[perms[i]] = perms[perms[i]], perms[i]
+
+        return s
+
 s = Solution()
 result = s.permute_elements_array_storage([2,0,1,3], ['a', 'b', 'c', 'd'])
 print(result)
-
-
-
-
-
-
