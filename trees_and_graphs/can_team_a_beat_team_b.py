ae25ef2bc3c88d342999538f94112ceef9fa9745
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-10-04 11:33:05 -0500

    Adding comments and ref to Kahn's algorithm

--- trees_and_graphs/can_team_a_beat_team_b.py
+++ trees_and_graphs/can_team_a_beat_team_b.py
@@ -1,4 +1,4 @@
-# EPI 18.0
+# EPI Graphs 18.0
 """
 Given a list of outcomes of matches between pairs of teams, with each outcome being a win or a loss,
 Can team A beat B i.e. is there a sequence of teams starting with A and ending with B such that each team in the sequence 
@@ -23,11 +23,14 @@ def can_team_a_beat_team_b(matches, team_a, team_b):
             graph[match.winning_team].add(match.losing_team)
         return graph
 
+    # Python's default arguments are evaluated once when the function is defined.
+    # Maintain state between calls of a function
+    # https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
     def is_reachable_dfs(graph, curr, dest, visited=set()):
         if curr == dest:
             return True
         # This is a cycle
-        # We reached a sink and cannot proceed.. no path forward
+        # We reached a leaf node -- no outgoing edges
         elif curr in visited or curr not in graph:
             return False
         visited.add(curr)
@@ -40,8 +43,9 @@ matches = []
 matches.append(MatchResult('A','C'))
 matches.append(MatchResult('C','D'))
 matches.append(MatchResult('D','E'))
-#matches.append(MatchResult('D','A'))
-matches.append(MatchResult('E','F'))
+matches.append(MatchResult('E','C'))   # Cycle no a DAG
+#matches.append(MatchResult('E','Z'))  # Leaf
+#matches.append(MatchResult('E','F'))
 matches.append(MatchResult('F','B'))
 
 
