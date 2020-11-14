1299273af146c9b2f7f0ea6304505666ec355a43
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-11-14 11:37:03 -0600

    LC 169 - Using Boyer Moore Majority Vote Algorithm

--- hash_tables/majority_element.py
+++ hash_tables/majority_element.py
@@ -1,18 +1,17 @@
 # LC 169. Majority Element https://leetcode.com/problems/majority-element/
-
-from collections import defaultdict
+# https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        result = 0
-        max_freq = 0
-        freq_counter = defaultdict(int)
+        majority_value = 0
+        majority_count = 0
         
-        for n in nums:
-            freq_counter[n] += 1
-            
-            if freq_counter[n] > max_freq:
-                max_freq = freq_counter[n]
-                result = n
-        return result
-            
+        for x in nums:
+            if majority_count == 0:
+                majority_value = x
+                majority_count = 1
+            elif majority_value == x:
+                majority_count += 1
+            else:
+                majority_count -= 1
+        return majority_value
