f3934519fd7a5c9cf4dfdc7e39993d4f3619d155
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-11-27 16:36:23 -0600

    LC 687 - Added namedtuple

--- trees_and_graphs/longest_univalue_path.py
+++ trees_and_graphs/longest_univalue_path.py
@@ -6,32 +6,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import namedtuple
 class Solution:
     def __init__(self):
         self.max_path = 0
         
     def longestUnivaluePath(self, root: TreeNode) -> int:
+        Status = namedtuple('Status', ['value', 'path'])
         def helper(root):
             if root is None:
-                return (float('-inf'), 0)
+                return Status(value=float('-inf'), path=0)
+
             left_status = helper(root.left)
             right_status = helper(root.right)
             
             my_path = 0
             subtree_path = 0
             
-            if left_status[0] == root.val and right_status[0] == root.val:
-                subtree_path = left_status[1] + 1 + right_status[1] + 1
-                my_path = max(left_status[1], right_status[1]) + 1
-            elif left_status[0] == root.val:
-                subtree_path = left_status[1] + 1
-                my_path = subtree_path
-            elif right_status[0] == root.val:
-                subtree_path = right_status[1] + 1
-                my_path = subtree_path
-                
+            if left_status.value == root.val and right_status.value == root.val:
+                subtree_path = left_status.path + 1 + right_status.path + 1
+                my_path = max(left_status.path, right_status.path) + 1
+            elif left_status.value == root.val:
+                my_path = subtree_path = left_status.path + 1
+            elif right_status.value == root.val:
+                my_path = subtree_path = right_status.path + 1
+     
             self.max_path = max(self.max_path, subtree_path)    
-            return (root.val, my_path)
+            return Status(value=root.val, path=my_path)
         
         helper(root)
         return self.max_path
