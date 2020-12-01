39c718eb48c7de25bb375bea8fd3d8d49dee4740
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-12-01 15:51:47 -0600

    HR Tree: Height of a Binary Tree

---
+++ trees_and_graphs/height_BST.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    d = {'height': 0}
+    # Postorder traversal
+    def height_helper(root, d, depth):
+        if root == None:
+            return
+        
+        # Leaf node
+        height_helper(root.left, d, depth + 1)
+        height_helper(root.right, d, depth + 1)
+        d['height'] = max(depth, d['height'])
+
+    height_helper(root, d, 0)
+    print(d)
+    return d['height']
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
+
+
