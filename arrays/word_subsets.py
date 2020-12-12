2dc72ffcc7863367405ee87eb4d74471884309ce
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2020-12-12 15:13:50 -0600

    LC 916. Word Subsets

--- arrays/word_subsets.py
+++ arrays/word_subsets.py
@@ -3,39 +3,44 @@
 from collections import Counter
 from typing import List
 
-
 """
 O(A + B) time
 O(1) space
 """
-class Solution:
-    def getRequiredCharCounts(self, B: List[str]):
-        """
-            Returns the minimum count of characters a string should contain to be considered universal
-        """
-        reqdCharCounts = Counter('')
-        for b in B:
-            bCounts = Counter(b)
-            for letter in bCounts:
-                reqdCharCounts[letter] = max(reqdCharCounts[letter], bCounts[letter])
-        return reqdCharCounts
 
-    
-    def findUniversalStrings(self, A, reqdCharCounts):
-        """
-            Returns the subset of strings in A who meet the count of characters specified
-        """
-        results = []
-        for a in A:
-            aCounts = Counter(a)
-            for letter in reqdCharCounts:
-                if aCounts[letter] < reqdCharCounts[letter]:
-                    break
-            else:
-                results.append(a)
-        return results
+def getRequiredCharCounts(B: List[str]):
+    """
+        Returns the minimum count of characters a string should contain to be considered universal
+    """
+    reqdCharCounts = Counter('')
+    for b in B:
+        bCounts = Counter(b)
+        for letter in bCounts:
+            reqdCharCounts[letter] = max(reqdCharCounts[letter], bCounts[letter])
+    return reqdCharCounts
 
-    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
-        reqdCharCounts = self.getRequiredCharCounts(B)
-        results = self.findUniversalStrings(A, reqdCharCounts)
-        return results
+
+def findUniversalStrings(A, reqdCharCounts):
+    """
+        Returns the subset of strings in A that meet the count of characters specified
+    """
+    results = []
+    for a in A:
+        aCounts = Counter(a)
+        for letter in reqdCharCounts:
+            if aCounts[letter] < reqdCharCounts[letter]:
+                break
+        else:
+            results.append(a)
+    return results
+
+def wordSubsets(A: List[str], B: List[str]) -> List[str]:
+    """
+    >>> wordSubsets(["amazon","apple","facebook","google","leetcode"], ["l","e"])
+    ['apple', 'google', 'leetcode']
+    >>> wordSubsets(["amazon","apple","facebook","google","googllle","leetcode"], ["lll","e"])
+    ['googllle']
+    """
+    reqdCharCounts = getRequiredCharCounts(B)
+    results = findUniversalStrings(A, reqdCharCounts)
+    return results
