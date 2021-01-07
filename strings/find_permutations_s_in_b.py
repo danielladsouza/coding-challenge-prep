035576e57a8618c6548681998c39bfcac7365111
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-01-07 16:58:04 -0600

    Code Cleanup

--- strings/find_permutations_s_in_b.py
+++ strings/find_permutations_s_in_b.py
@@ -17,12 +17,15 @@ def find_all_permutations(s: str, b:str) -> List[str]:
 
     # Use the concept of anagram . Counter for s
     # Compare two counters if there is a match , we have a valid permutation.
+    Time complexity O(B)
+    Space complexity O(B)
     """
     window_size = len(s)
     result = []
 
     d = Counter(s)
-    g = (b[i:i+window_size] for i in range(0, len(b) - window_size -1))
+
+    g = (b[i:i+window_size] for i in range(0, len(b) - window_size + 1))
     for y in g:
         c = Counter(y)
         if c == d:
@@ -31,5 +34,7 @@ def find_all_permutations(s: str, b:str) -> List[str]:
 
 
 assert(len(find_all_permutations('elvis', 'elvisliveselvisliveselvislive'))
-       == 9)
+       == 10)
 
+assert(len(find_all_permutations('a', 'aaaaaaaaaa'))
+       == 10)
