c4224b3aa4184ccd278d3b59853abf7c0f8e0fe5
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-01-08 11:23:28 -0600

    PR Decrypt Messgae - simplifying the decryption algorithm

---
+++ strings/decrypt_message.py
@@ -0,0 +1,39 @@
+"""
+    decrypt_message.py
+    Messages consist of lowercase latin letters only, and every word is
+    encrypted separately as follows:
+
+    Convert every letter to its ASCII value. Add 1 to the first letter, and
+    then for every letter from the second one to the last one, add the value
+    of the previous letter. Subtract 26 from every letter until it is in the
+    range of lowercase letters a-z in ASCII. Convert the values back to
+    letters.
+"""
+
+
+def decrypt(word):
+    sub_value = 1
+    decrypted = ''
+    # For improved space complexity we are manipulating one character at a time
+    for c in word:  # TC O(N)
+        # Keep track of the previous value
+        prev_value = ascii_value = ord(c)
+        # Manipulation of the data
+        ascii_value -= sub_value
+        # sub_value += prev_value
+        # Normalization Step
+        while ascii_value < ord('a'):  # TC O(1)
+            ascii_value += 26
+
+        # The previous value becomes the new value we need to subtract
+        sub_value = prev_value
+        decrypted += chr(ascii_value)  # SC O(N)
+
+    return decrypted
+
+
+assert (decrypt('dnotq') == 'crime')
+assert (decrypt('flgxswdliefy') == 'encyclopedia')
+print("TC O(N) SC O(a)")
+
+
