01b6ec027d6bc4f31732ca857101705f8ff91fd9
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-04-22 16:27:48 -0500

    Use string.digits.index() to convert char to int (digit)

--- strings/string_to_int.py
+++ strings/string_to_int.py
@@ -3,28 +3,19 @@
     the corresponding integer
     Code should handle negative values
 """
+import string
 
 
 def string_to_int(s:str) -> int:
     """ Time complexity O(N)
         Space complexity O(1)"""
     # Parse the string character by character. Build up the corresponding int
-    # Use the ord, chr for conversion.
-    lookup = dict((chr(ord('0') + i), i) for i in range(10))
-    # dictionary - '0': 0...'9':9
-    result, negative = 0, False
+    result = 0
 
-    for c in s:
-        if c == '-':
-            negative = True
-            continue
+    for c in s[s[0] == '-':]:
+        result = string.digits.index(c) + result * 10
 
-        result = lookup[c] + result * 10
-
-    if negative and result:
-        result = -result
-
-    return result
+    return result * (-1 if s[0] == '-' else 1)
 
 
 r = string_to_int("123")
