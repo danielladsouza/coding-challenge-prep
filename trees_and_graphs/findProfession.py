1a429e4db2c8c38d7294b6c36879a5f1213d3fd9
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-07-19 11:58:33 -0500

    CS Trees - Updated explanation

--- trees_and_graphs/findProfession.py
+++ trees_and_graphs/findProfession.py
@@ -4,11 +4,24 @@ def findProfession(level, pos):
     1. The profession of his parent
     2. If it is the left or the right child of the parent which translates to whether
        it is at an odd or even position
-       
-      1
-     / \
-    1   2
-    Parent at pos , children at position (pos, 2* pos)  
+
+    Key here is to recursively figure out the profession of the parent.
+                      E
+                     /  \
+                    E    D
+                   / \  / \
+                  E   D D  E
+
+    Once I know the profession of the parent, if I am at an odd position, my profession is that of my parent
+    If I am at an even position my profession is different from that of my parent
+
+    Assuming posiitons are 1 based,
+    Child at position, pos
+    Parent at position (pos + 1)  // 2
+
+    Parent at position , pos
+    Children at posiiton -   2*pos -1, 2 * pos
+
     Edge cases
     level = 1
     level = 2, pos = 2
