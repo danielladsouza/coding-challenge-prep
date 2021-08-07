8457133427d84052122daed2b620c9b994b234cd
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-08-07 14:21:54 -0500

    LC 211 Fixing a typo, adding comments

--- trees_and_graphs/design_add_search_words.py
+++ trees_and_graphs/design_add_search_words.py
@@ -11,12 +11,16 @@ class WordDictionary:
     """
         A Tree data structure that contains only lowercase letters
         Every node is a word dictionary
+        DFS - Preorder Traversal
     """
     def __init__(self, c = '\0'):
         """
         Initialize your data structure here.
         """
-        self.c = c
+        self.c = c   # This is not required as the position of the WordDictionary in the list of children can tell you it's ASCII character
+                     # index 0 - the letter 'a', index 25 - the letter 'z'
+                     # Useful if you want to provide str or repr
+
         self.children = [None] * ASCII_LOWERCASE_CHARS
         self.isWord = False
 
@@ -34,17 +38,33 @@ class WordDictionary:
 
         for idx, w in enumerate(word):
             remainder = word[idx + 1:]
-            if w[idx] == '.':   # All the children are possible candidates for this character
+            if w == '.':   # All the children are possible candidates for this character
                 for c in self.children:
-                    if c and c.search(remainder):
+                    if c and c.search(remainder):  # c could be None, we have preallocated an array of Nones
                         return True
                 return False
             else:
-                c = self.children[ord(w[idx]) - ord('a')]
+                c = self.children[ord(w) - ord('a')]
                 if c:
                     return c.search(remainder)
                 else:
                     return False
+
+"""
+car 
+0 c car
+        ar
+c Node as root
+look for 
+a    ar
+        r
+        
+a Node as root
+look for r
+    
+r Node as root word is now empty
+r Node isWord
+"""
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
