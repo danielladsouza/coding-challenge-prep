066f7edcc17195d97c5f441f9b97010fe5cfd78f
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-08-07 12:53:19 -0500

    LC 211 - Refactoring - WordDictionary

--- trees_and_graphs/design_add_search_words.py
+++ trees_and_graphs/design_add_search_words.py
@@ -7,84 +7,46 @@
 """
 ASCII_LOWERCASE_CHARS = 26
 
-class Node:
-    def __init__(self, c: str):
-        self.c = c
-        self.children = [None] * ASCII_LOWERCASE_CHARS
-        self.isWord = False
-        print(c)
-
-
 class WordDictionary:
     """
         A Tree data structure that contains only lowercase letters
+        Every node is a word dictionary
     """
-
-    def __init__(self):
+    def __init__(self, c = '\0'):
         """
         Initialize your data structure here.
         """
-        self.root = Node('\0')
+        self.c = c
+        self.children = [None] * ASCII_LOWERCASE_CHARS
+        self.isWord = False
 
     def addWord(self, word: str) -> None:
-        curr = self.root
+        curr = self
         for w in word:
             if not curr.children[ord(w) - ord('a')]:
-                curr.children[ord(w) - ord('a')] = Node(
-                    w)  # Inserting a new node with this character
+                curr.children[ord(w) - ord('a')] = WordDictionary(w)  # Inserting a new node with this character
             curr = curr.children[ord(w) - ord('a')]
         curr.isWord = True
 
     def search(self, word: str) -> bool:
+        if word == "": # Exhausted the string
+            return self.isWord
 
-        def getWord(curr, idx, w: str) -> Node:
-            # Base case
-
-            # Sanity check
-            if idx == len(w) or not curr:
+        for idx, w in enumerate(word):
+            remainder = word[idx + 1:]
+            if w[idx] == '.':   # All the children are possible candidates for this character
+                for c in self.children:
+                    if c and c.search(remainder):
+                        return True
                 return False
-
-            # Reached the end of the word
-            if idx == len(w) - 1 and (curr.c == w[idx] or w[idx] == '.'):
-                if curr.isWord:
-                    return True
-
-            # When to return success
-            # When to continue the DFS
-            # when to give up.
-            # Pre order traversal - Root and then children
-
-            if curr.c == '\0':
-                for c in curr.children:
-                    finalNode = getWord(c, idx, w)
-                    if finalNode:
-                        return True
-            elif w[idx] == '.' or curr.c == w[idx]:
-                for c in curr.children:
-                    foundWord = getWord(c, idx + 1, w)
-                    if foundWord:
-                        return True
-
-            return False
-
-        return getWord(self.root, 0, word)
-        """
-        curr = self.root
-        for w in word:
-            if w is '.':
-                continue
-            if not curr.children[ord(w) - ord('a')]:
-                return None
-            curr = curr.children[ord(w) - ord('a')]
-        return curr.isWord
-        """
+            else:
+                c = self.children[ord(w[idx]) - ord('a')]
+                if c:
+                    return c.search(remainder)
+                else:
+                    return False
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
-
-# ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
-# [[],["bad"],["dad"],["mad"],["pad"],["bad"],["dad"],["bad"]]
-
-# [null,null,null,null,false,true,true,true]
