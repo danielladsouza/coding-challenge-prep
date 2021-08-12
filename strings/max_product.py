b500021237b10ab2fc25f8715c2c942842287bd4
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-08-12 17:09:47 -0500

    LC 318 - Maximum Product of Word Lengths

--- strings/max_product.py
+++ strings/max_product.py
@@ -2,7 +2,6 @@
 
 from collections import Counter
 
-
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
         """
@@ -14,6 +13,11 @@ class Solution:
             calculat the product
             Track the max_product
 
+            Brute Force approach
+            1000 words each upto 1000 in length
+            TC O(N**2)
+            SC O(N)
+
         """
         max_product = 0
         words_counter = [None] * len(words)
@@ -22,10 +26,46 @@ class Solution:
 
         for i in range(len(words_counter) - 1):
             for j in range(i + 1, len(words_counter)):
-                for key in words_counter[i]:
+                for key in words_counter[i]:  # We have to iterate over each of the key letters
                     if key in words_counter[j]:
                         break
                 else:
+                    max_product = max(max_product,
+                                      len(words[i]) * len(words[j]))
+        return max_product
+
+    def maxProduct(self, words: List[str]) -> int:
+        """
+            1. Find out words that do not share common letters
+            word into a Counter for comparison
+            2. T.C. O(N**2)  - Brute Force approach
+
+            Iterate over the words comparing bitmasks for word[i] with word[j]. If nothing in common,
+            calculate the product. This saves us from having to compare each individual character in the word
+            THe length of each word can extend upto 1000 characters, even using a dictionary we would need to
+            aggregate the data and then compare.
+
+            Track the max_product
+
+            Brute Force approach
+            1000 words each upto 1000 in length
+            TC O(N**2)
+            SC O(N)
+
+        """
+        def build_bitmask(word: str) -> int:
+            bitmask = 0
+            for char in word:
+                bitmask = bitmask | (1 << (ord(char) - ord('a')))
+
+            return bitmask
+
+        max_product = 0
+        words_bitmask = list(map(build_bitmask, words))
+
+        for i in range(len(words_bitmask) - 1):
+            for j in range(i + 1, len(words_bitmask)):
+                if words_bitmask[i] & words_bitmask[j] == 0:
                     max_product = max(max_product,
                                       len(words[i]) * len(words[j]))
         return max_product
