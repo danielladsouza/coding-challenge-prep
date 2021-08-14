56ccdca9d5834f5e2367adc15957d3808c2ab417
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-08-14 06:50:06 -0500

    LC 318. Maximum Product of Word Lengths

--- strings/max_product.py
+++ strings/max_product.py
@@ -1,4 +1,11 @@
 # L.C. 318 - Maximum Product of Word Lengths
+"""
+    Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters.
+    If no such two words exist, return 0.
+    Input: words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+    Output: 16
+    Explanation: The two words can be "abcw", "xtfn".
+"""
 
 from collections import Counter
 
@@ -52,20 +59,29 @@ class Solution:
             TC O(N**2)
             SC O(N)
 
+            Bitmasks are useful anytime you need to keep track of which of N items is in a group. the group is represented by a N digit binary number,
+            and the ith bit is 1 if the item is present and 0 otherwise. We can therefore see where two groups share items by taking the and of their masks;
+            the remaining bits are the shared items. Remember that left shifting by i is an easy way to get the 2^i, the ith digit.
+            Once we have the bitmasks the rest of the problem is very basic; as long as the and of the two word bitmasks is 0 then we have a valid pair.
+
         """
         def build_bitmask(word: str) -> int:
+            """
+                Build a 26 bit binary number
+            """
             bitmask = 0
             for char in word:
                 bitmask = bitmask | (1 << (ord(char) - ord('a')))
-
             return bitmask
 
         max_product = 0
-        words_bitmask = list(map(build_bitmask, words))
+        bitmask_words = list(map(build_bitmask, words))
 
-        for i in range(len(words_bitmask) - 1):
-            for j in range(i + 1, len(words_bitmask)):
-                if words_bitmask[i] & words_bitmask[j] == 0:
+        for i in range(len(words) - 1):
+            for j in range(i + 1, len(words)):
+                # Do the comparison using the bitmasked version of the words
+                # Nothing in common will yield 0 for an and
+                if bitmask_words[i] & bitmask_words[j] == 0:
                     max_product = max(max_product,
                                       len(words[i]) * len(words[j]))
         return max_product
