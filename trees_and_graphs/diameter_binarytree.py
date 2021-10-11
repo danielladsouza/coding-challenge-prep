5119ef1fd0b53efee41631e5f8016392f1624f9f
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-10-10 21:24:15 -0500

    LC 543 - Diameter of Binary Tree

--- trees_and_graphs/diameter_binarytree.py
+++ trees_and_graphs/diameter_binarytree.py
@@ -1,49 +1,33 @@
+# LC 543 - Diameter of Binary Tree
+
 # Definition for a binary tree node.
-# LC 543
-# [1,2,3,4,5]
-"""
-    Height of a binary tree is the maximum depth of any node in that tree
-
-    Depth of a node is the longest path from the root to that node not
-    including the root
-    Height of a node in a binary tree =
-    The max of the height of it's left subtree and the right subtree + 1
-    A successor (left/right node)  is always one edge away from it's ancestor
-"""
-
-from collections import defaultdict
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def __init__(self):
-        self.diameter = 0
-        
-    def diameter_of_binary_tree(self, root: TreeNode) -> int:
-        """
-            Time Complexity - log(N)
-            Space Complexity - Log(1)
-        """
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        longest_path = float('-inf')
 
-        if root is None:
-            return 0
-        
-        def height(node: TreeNode) -> int:
-            if root is None:
+        def helper(node: TreeNode):
+            # The nonlocal keyword is used to work with variables inside nested functions,
+            # where the variable should not belong to the inner function.
+
+            nonlocal longest_path
+
+            if node == None:
                 return 0
-            
-            left_height = height(node.left)
-            right_height = height(node.right)
-            diameter = left_height + right_height
-            self.diameter = max(diameter, self.diameter)
-            # We are ascending one level / endge
-            return 1 + max(left_height, right_height)
-            
-        height(root)
-        return self.diameter
 
+            left_path = helper(node.left)
+            right_path = helper(node.right)
+
+            # 1. Longest path with the current node as the root
+            local_path = left_path + right_path
+            longest_path = max(longest_path, local_path)
+
+            # 2. Return to my ancestor the longest path passing through me
+            return max(left_path, right_path) + 1
+
+        helper(root)
+        return longest_path
