a7d820a7d0ebace2daaceb67a67afacffbe2e1ff
Daniella DSouza <daniella.dsouza@heritageroboticsacademy.com>
2021-11-18 09:00:10 -0600

    LC 199 Refactored

--- trees_and_graphs/binary_tree_right_side_view.py
+++ trees_and_graphs/binary_tree_right_side_view.py
@@ -11,35 +11,39 @@
 # Time complexity - O(N)
 # Space Complexity - O(N) - If all nodes were on the same level
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 from collections import deque
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-
-        """
-            BFS - deque - FIFO
-        """
         if not root:
             return []
 
+        result = []
         bfs_queue = deque([root])
+        count = 0
+        while bfs_queue:
+            count = len(bfs_queue)
+            current_node = last_node = None
 
-        def level_order_traversal(node):
-            while (bfs_queue):
-                count = len(bfs_queue)
-                current_node = None
-                # Get the last value at the current level
-                last_value = float('-inf')
-                while count:
-                    current_node = bfs_queue.popleft()
-                    last_value = current_node.val
-                    if current_node.left:
-                        bfs_queue.append(current_node.left)
-                    if current_node.right:
-                        bfs_queue.append(current_node.right)
-                    count -= 1
-                result.append(last_value)
+            while count:
+                current_node = bfs_queue.popleft()
+                if current_node.left:
+                    bfs_queue.append(current_node.left)
+                if current_node.right:
+                    bfs_queue.append(current_node.right)
 
-        level_order_traversal(root)
+                last_node = current_node
+                count -= 1
+
+            if last_node:
+                result.append(last_node.val)
 
         return result
